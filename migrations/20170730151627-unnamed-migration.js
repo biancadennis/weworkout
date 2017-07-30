fa743a24021b714d8790b8eb88a1322bc9789b31
Bianca Dennis <bianca.dennis@caa.columbia.edu>
2017-07-30 11:32:57 -0400

    created postgres database

---
+++ migrations/20170730151627-unnamed-migration.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return(queryInterface.createTable('users', {
+      id: {
+        type:          Sequelize.INTEGER,
+        primaryKey:    true,
+        autoIncrement: true,
+        allowNull:     false
+      },
+      location: {
+        type:          Sequelize.INTEGER,
+        allowNull:     false
+      },
+      email: {
+        type:      Sequelize.STRING,
+        allowNull: false
+      },
+      fitnesslevel: {
+        type:      Sequelize.STRING,
+        allowNull: false
+      },
+      gender: {
+        type:      Sequelize.STRING,
+        allowNull: false
+      },
+      goals: {
+        type:      Sequelize.STRING,
+        allowNull: false
+      },
+      username: {
+        type:      Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type:      Sequelize.STRING,
+        allowNull: false
+      },
+      name: {
+        type:      Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type:      Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type:      Sequelize.DATE,
+        allowNull: false
+      }
+    }));
+  },
+
+  down: function(queryInterface, Sequelize) {
+    return(queryInterface.dropTable('users'));
+  }
+};
---
+++ models/users.js
@@ -0,0 +1,83 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('user', {
+    email: {
+      type:      DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: {
+          msg: 'Email is not a valid email address'
+        },
+        isUnique: function(value, next) {
+          var self = this;
+
+          User.findOne({
+            where: {
+              email: value
+            }
+          }).then(function(user) {
+            if (user && user.id != self.id)
+              return(next('Email is already registered'));
+            else
+              return(next());
+          });
+        }
+      }
+    },
+    username: {
+      type:      DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUnique: function(value, next) {
+          var self = this;
+
+          User.findOne({
+            where: {
+              username: value
+            }
+          }).then(function(user) {
+            if (user && user.id != self.id)
+              return(next('username is already registered'));
+            else
+              return(next());
+          });
+        }
+      }
+    },
+    bio: {
+        type:      DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'A Bio is required. Tell us a little about yourself.'
+            }
+        }
+	},
+    password: {
+      type:      DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [{min: 8}],
+          msg: 'Password must be at least 8 characters'
+        }
+      }
+    },
+    name: {
+      type:      DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Name is required'
+        }
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        models.user.hasMany(models.matches);
+      }
+    }
+  })
+
+  return(User);
+};
